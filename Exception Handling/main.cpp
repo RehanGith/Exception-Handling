c520b0a17ea52aff44417f60de627efb2c8cd367
RehanGith <chrehan1007@gmail.com>
2024-05-17 00:11:18 +0500

    an exception handling function

---
+++ Exception Handling/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+double division(int nominator, int dinominoter) {
+	double n = 0.0;
+	try {
+		if (dinominoter == 0)
+			throw 0;
+		else
+			n = nominator / dinominoter;
+	}
+	catch (int& ex) {
+		cerr << "Dinominoter is zero" << endl;
+	}
+	cout << "Program continue" << endl;
+	return n;
+}
+
+int main() {
+
+}
